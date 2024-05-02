2e66fd2fd9246ccc71e720c8e046f51cc8f0486f
paul moore <paulmoore100@hotmail.com>
2024-05-02 16:32:56 -0700

    remove ASCII_xx

--- ice40/hex_ascii.v
+++ ice40/hex_ascii.v
@@ -3,12 +3,12 @@
 function [3:0] f_ASCII_To_Hex;
   input [7:0] i_ASCII;
   begin
-    if (i_ASCII == ASCII_a || i_ASCII == ASCII_A) f_ASCII_To_Hex = 4'hA;
-    else if (i_ASCII == ASCII_b || i_ASCII == ASCII_B) f_ASCII_To_Hex = 4'hB;
-    else if (i_ASCII == ASCII_c || i_ASCII == ASCII_C) f_ASCII_To_Hex = 4'hC;
-    else if (i_ASCII == ASCII_d || i_ASCII == ASCII_D) f_ASCII_To_Hex = 4'hD;
-    else if (i_ASCII == ASCII_e || i_ASCII == ASCII_E) f_ASCII_To_Hex = 4'hE;
-    else if (i_ASCII == ASCII_f || i_ASCII == ASCII_F) f_ASCII_To_Hex = 4'hF;
+    if (i_ASCII == "a" || i_ASCII == "A") f_ASCII_To_Hex = 4'hA;
+    else if (i_ASCII == "b" || i_ASCII == "B") f_ASCII_To_Hex = 4'hB;
+    else if (i_ASCII == "c" || i_ASCII == "C") f_ASCII_To_Hex = 4'hC;
+    else if (i_ASCII == "d" || i_ASCII == "D") f_ASCII_To_Hex = 4'hD;
+    else if (i_ASCII == "e" || i_ASCII == "E") f_ASCII_To_Hex = 4'hE;
+    else if (i_ASCII == "f" || i_ASCII == "F") f_ASCII_To_Hex = 4'hF;
     else f_ASCII_To_Hex = i_ASCII[3:0];
   end
 endfunction
@@ -18,12 +18,12 @@ endfunction
 function [7:0] f_Hex_To_ASCII;
   input [3:0] i_Hex;
   begin
-    if (i_Hex == 4'hA) f_Hex_To_ASCII = ASCII_a;
-    else if (i_Hex == 4'hB) f_Hex_To_ASCII = ASCII_b;
-    else if (i_Hex == 4'hC) f_Hex_To_ASCII = ASCII_c;
-    else if (i_Hex == 4'hD) f_Hex_To_ASCII = ASCII_d;
-    else if (i_Hex == 4'hE) f_Hex_To_ASCII = ASCII_e;
-    else if (i_Hex == 4'hF) f_Hex_To_ASCII = ASCII_f;
+    if (i_Hex == 4'hA) f_Hex_To_ASCII = ASCII_"a";
+    else if (i_Hex == 4'hB) f_Hex_To_ASCII = "b";
+    else if (i_Hex == 4'hC) f_Hex_To_ASCII = "c";
+    else if (i_Hex == 4'hD) f_Hex_To_ASCII = "d";
+    else if (i_Hex == 4'hE) f_Hex_To_ASCII = "e";
+    else if (i_Hex == 4'hF) f_Hex_To_ASCII = "f";
     else f_Hex_To_ASCII = {4'h3, i_Hex[3:0]};
   end
 endfunction
